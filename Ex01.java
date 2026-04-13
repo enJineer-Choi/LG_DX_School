aa1e25fa530eb96b00362a1ee0d1bf8b0561e390
최석진 (Choe Seok Jin) <54811217+enJineer-Choi@users.noreply.github.com>
2026-04-13 18:49:46 +0900

    java file upload

---
+++ Ex01.java
@@ -0,0 +1,27 @@
+package ex0408;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// 주석 
+		
+		// TODO > 내가 해야할일 > 어디에서 뭐할지를 todo를 활용할 수 있음	
+		
+		//ctrl + d > 한줄을 한번에 지우는 코드
+		
+		//{} 시작부분({)을 더블클릭하면 닫히는 곳까지 포함하는 전체 범위를 알려줌
+		
+		// 앞으로 코드는 main method안에서 작성
+		
+		// 출력문
+		System.out.println("Hello world!");
+		
+		// 단축키 sysout > ctrl + space
+		System.out.println("hi");
+		// 실행 단축키 ctrl + f11
+		
+		// Java는 컴파일러 언어 >
+		System.out.println("hi");
+	}
+
+}
